7529432b4f6b713ce81561385fe51b2ea8ad248e
Sami Islam <islam.samipkr@gmail.com>
2024-01-26 06:51:57 -0500

    Update c1-versions.tf

--- c1-versions.tf
+++ c1-versions.tf
@@ -26,3 +26,9 @@ provider "aws" {
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
 $HOME/.aws/credentials
 */
+variable "AWS_ACCESS_KEY"{
+  
+}
+variable "AWS_SECRET_ACCESS_KEY"{
+  
+}
